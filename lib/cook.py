e37f3e78a6b1b4941fccfb347e548473a2df11cb
mdbartos <mdbartos@umich.edu>
2018-12-30 21:01:13 +0000

    Add runfile

---
+++ lib/cook.py
@@ -0,0 +1,111 @@
+from cookbook import Cookbook
+
+print('Mary Beth Doyle...')
+mbd = Cookbook('../example/mary-beth-doyle.jsonld')
+mbd.run()
+del mbd
+
+print('Ellsworth South Inlet...')
+ells_s = Cookbook('../example/ellsworth_south_inlet.jsonld')
+ells_s.run()
+del ells_s
+
+print('Ellsworth North Inlet...')
+ells_n = Cookbook('../example/ellsworth_north_inlet.jsonld')
+ells_n.run()
+del ells_n
+
+print('Flagstar Bank...')
+flagstar = Cookbook('../example/flagstar-bank.jsonld')
+flagstar.run()
+del flagstar
+
+print('Hilton Garden Inn...')
+hilton = Cookbook('../example/hilton-garden-inn.jsonld')
+hilton.run()
+del hilton
+
+print('Kapnick Insurance Group...')
+kapnick = Cookbook('../example/kapnick-insurance-group.jsonld')
+kapnick.run()
+del kapnick
+
+print('Holiday Inn Flume...')
+holiday = Cookbook('../example/holiday-inn-flume.jsonld')
+holiday.run()
+del holiday
+
+print('Ellsworth Valve...')
+ells_v = Cookbook('../example/ellsworth-valve.jsonld')
+ells_v.run()
+del ells_v
+
+print('County Farm Park Valve...')
+cfp = Cookbook('../example/county-farm-park-valve.jsonld')
+cfp.run()
+del cfp
+
+print('Chalmers Drain...')
+chalmers = Cookbook('../example/chalmers-drain.jsonld')
+chalmers.run()
+del chalmers
+
+print('Kensington...')
+kens = Cookbook('../example/kensington.jsonld')
+kens.run()
+del kens
+
+print('North Territorial...')
+nt = Cookbook('../example/north-territorial.jsonld')
+nt.run()
+del nt
+
+print('Churchill Downs South...')
+cds = Cookbook('../example/churchill-downs-south.jsonld')
+cds.run()
+del cds
+
+print('Sycamore @ Sycamore School...')
+sycamore = Cookbook('../example/sycamore-sycamore_school.jsonld')
+sycamore.run()
+del sycamore
+
+print('Edgecliff @ Alta Mesa...')
+edge = Cookbook('../example/edgecliff-alta_mesa.jsonld')
+edge.run()
+del edge
+
+print('Sycamore @ Vickery...')
+vick = Cookbook('../example/sycamore-vickery.jsonld')
+vick.run()
+del vick
+
+print('Wildcat Branch @ Stalcup...')
+wildcat = Cookbook('../example/wildcat-branch-stalcup.jsonld')
+wildcat.run()
+del wildcat
+
+print('Fossil Creek @ Meacham...')
+fossil = Cookbook('../example/little-fossil-creek-meacham.jsonld')
+fossil.run()
+del fossil
+
+print('Whites Branch @ Basswood...')
+whites = Cookbook('../example/whites-branch-basswood.jsonld')
+whites.run()
+del whites
+
+print('South Marys Creek @ Lost Creek...')
+mary = Cookbook('../example/south_marys_creek_lost_creek.jsonld')
+mary.run()
+del mary
+
+print('Arlington @ Pecan')
+ar1 = Cookbook('../example/arlington_pecan.jsonld')
+ar1.run()
+del ar1
+
+print('Arlington @ Mitchell')
+ar3 = Cookbook('../example/arlington_mitchell.jsonld')
+ar3.run()
+del ar3
