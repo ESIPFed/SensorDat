5776646543fd94615f91a5b7401fa37ecc5f67c1
mdbartos <mdbartos@umich.edu>
2019-11-03 06:55:56 +0000

    Add updated commands from server

--- lib/cook.py
+++ lib/cook.py
@@ -1,286 +1,286 @@
 from cookbook import Cookbook
 
-print('Mary Beth Doyle...')
-mbd = Cookbook('../example/mary-beth-doyle.jsonld')
+print('Mary Beth Doyle/home/ubuntu/git/SensorDat.')
+mbd = Cookbook('/home/ubuntu/git/SensorDat/example/mary-beth-doyle.jsonld')
 mbd.run()
 del mbd
 
-print('Ellsworth South Inlet...')
-ells_s = Cookbook('../example/ellsworth_south_inlet.jsonld')
+print('Ellsworth South Inlet/home/ubuntu/git/SensorDat.')
+ells_s = Cookbook('/home/ubuntu/git/SensorDat/example/ellsworth_south_inlet.jsonld')
 ells_s.run()
 del ells_s
 
-print('Ellsworth North Inlet...')
-ells_n = Cookbook('../example/ellsworth_north_inlet.jsonld')
+print('Ellsworth North Inlet/home/ubuntu/git/SensorDat.')
+ells_n = Cookbook('/home/ubuntu/git/SensorDat/example/ellsworth_north_inlet.jsonld')
 ells_n.run()
 del ells_n
 
-print('Flagstar Bank...')
-flagstar = Cookbook('../example/flagstar-bank.jsonld')
+print('Flagstar Bank/home/ubuntu/git/SensorDat.')
+flagstar = Cookbook('/home/ubuntu/git/SensorDat/example/flagstar-bank.jsonld')
 flagstar.run()
 del flagstar
 
-print('Hilton Garden Inn...')
-hilton = Cookbook('../example/hilton-garden-inn.jsonld')
+print('Hilton Garden Inn/home/ubuntu/git/SensorDat.')
+hilton = Cookbook('/home/ubuntu/git/SensorDat/example/hilton-garden-inn.jsonld')
 hilton.run()
 del hilton
 
-print('Kapnick Insurance Group...')
-kapnick = Cookbook('../example/kapnick-insurance-group.jsonld')
+print('Kapnick Insurance Group/home/ubuntu/git/SensorDat.')
+kapnick = Cookbook('/home/ubuntu/git/SensorDat/example/kapnick-insurance-group.jsonld')
 kapnick.run()
 del kapnick
 
-print('Holiday Inn Flume...')
-holiday = Cookbook('../example/holiday-inn-flume.jsonld')
+print('Holiday Inn Flume/home/ubuntu/git/SensorDat.')
+holiday = Cookbook('/home/ubuntu/git/SensorDat/example/holiday-inn-flume.jsonld')
 holiday.run()
 del holiday
 
-print('Ellsworth Valve...')
-ells_v = Cookbook('../example/ellsworth-valve.jsonld')
+print('Ellsworth Valve/home/ubuntu/git/SensorDat.')
+ells_v = Cookbook('/home/ubuntu/git/SensorDat/example/ellsworth-valve.jsonld')
 ells_v.run()
 del ells_v
 
-print('County Farm Park Valve...')
-cfp = Cookbook('../example/county-farm-park-valve.jsonld')
+print('County Farm Park Valve/home/ubuntu/git/SensorDat.')
+cfp = Cookbook('/home/ubuntu/git/SensorDat/example/county-farm-park-valve.jsonld')
 cfp.run()
 del cfp
 
-print('Chalmers Drain...')
-chalmers = Cookbook('../example/chalmers-drain.jsonld')
+print('Chalmers Drain/home/ubuntu/git/SensorDat.')
+chalmers = Cookbook('/home/ubuntu/git/SensorDat/example/chalmers-drain.jsonld')
 chalmers.run()
 del chalmers
 
-print('Kensington...')
-kens = Cookbook('../example/kensington.jsonld')
+print('Kensington/home/ubuntu/git/SensorDat.')
+kens = Cookbook('/home/ubuntu/git/SensorDat/example/kensington.jsonld')
 kens.run()
 del kens
 
-print('North Territorial...')
-nt = Cookbook('../example/north-territorial.jsonld')
+print('North Territorial/home/ubuntu/git/SensorDat.')
+nt = Cookbook('/home/ubuntu/git/SensorDat/example/north-territorial.jsonld')
 nt.run()
 del nt
 
-print('Churchill Downs South...')
-cds = Cookbook('../example/churchill-downs-south.jsonld')
+print('Churchill Downs South/home/ubuntu/git/SensorDat.')
+cds = Cookbook('/home/ubuntu/git/SensorDat/example/churchill-downs-south.jsonld')
 cds.run()
 del cds
 
-print('Sycamore @ Sycamore School...')
-sycamore = Cookbook('../example/sycamore-sycamore_school.jsonld')
+print('Sycamore @ Sycamore School/home/ubuntu/git/SensorDat.')
+sycamore = Cookbook('/home/ubuntu/git/SensorDat/example/sycamore-sycamore_school.jsonld')
 sycamore.run()
 del sycamore
 
-print('Edgecliff @ Alta Mesa...')
-edge = Cookbook('../example/edgecliff-alta_mesa.jsonld')
+print('Edgecliff @ Alta Mesa/home/ubuntu/git/SensorDat.')
+edge = Cookbook('/home/ubuntu/git/SensorDat/example/edgecliff-alta_mesa.jsonld')
 edge.run()
 del edge
 
-print('Sycamore @ Vickery...')
-vick = Cookbook('../example/sycamore-vickery.jsonld')
+print('Sycamore @ Vickery/home/ubuntu/git/SensorDat.')
+vick = Cookbook('/home/ubuntu/git/SensorDat/example/sycamore-vickery.jsonld')
 vick.run()
 del vick
 
-print('Wildcat Branch @ Stalcup...')
-wildcat = Cookbook('../example/wildcat-branch-stalcup.jsonld')
+print('Wildcat Branch @ Stalcup/home/ubuntu/git/SensorDat.')
+wildcat = Cookbook('/home/ubuntu/git/SensorDat/example/wildcat-branch-stalcup.jsonld')
 wildcat.run()
 del wildcat
 
-print('Fossil Creek @ Meacham...')
-fossil = Cookbook('../example/little-fossil-creek-meacham.jsonld')
+print('Fossil Creek @ Meacham/home/ubuntu/git/SensorDat.')
+fossil = Cookbook('/home/ubuntu/git/SensorDat/example/little-fossil-creek-meacham.jsonld')
 fossil.run()
 del fossil
 
-print('Whites Branch @ Basswood...')
-whites = Cookbook('../example/whites-branch-basswood.jsonld')
+print('Whites Branch @ Basswood/home/ubuntu/git/SensorDat.')
+whites = Cookbook('/home/ubuntu/git/SensorDat/example/whites-branch-basswood.jsonld')
 whites.run()
 del whites
 
-print('South Marys Creek @ Lost Creek...')
-mary = Cookbook('../example/south_marys_creek_lost_creek.jsonld')
+print('South Marys Creek @ Lost Creek/home/ubuntu/git/SensorDat.')
+mary = Cookbook('/home/ubuntu/git/SensorDat/example/south_marys_creek_lost_creek.jsonld')
 mary.run()
 del mary
 
-print('Arlington @ Pecan...')
-ar1 = Cookbook('../example/arlington_pecan.jsonld')
+print('Arlington @ Pecan/home/ubuntu/git/SensorDat.')
+ar1 = Cookbook('/home/ubuntu/git/SensorDat/example/arlington_pecan.jsonld')
 ar1.run()
 del ar1
 
-print('Arlington @ Mitchell...')
-ar3 = Cookbook('../example/arlington_mitchell.jsonld')
+print('Arlington @ Mitchell/home/ubuntu/git/SensorDat.')
+ar3 = Cookbook('/home/ubuntu/git/SensorDat/example/arlington_mitchell.jsonld')
 ar3.run()
 del ar3
 
-print('Bowman Branch @ Mirabella...')
-bbm = Cookbook('../example/bowman_branch_mirabella.jsonld')
+print('Bowman Branch @ Mirabella/home/ubuntu/git/SensorDat.')
+bbm = Cookbook('/home/ubuntu/git/SensorDat/example/bowman_branch_mirabella.jsonld')
 bbm.run()
 del bbm
 
-print('Marsalis...')
-mar = Cookbook('../example/marsalis.jsonld')
+print('Marsalis/home/ubuntu/git/SensorDat.')
+mar = Cookbook('/home/ubuntu/git/SensorDat/example/marsalis.jsonld')
 mar.run()
 del mar
 
-print('Prairie Creek @ Bruton...')
-prb = Cookbook('../example/prarie-creek-bruton.jsonld')
+print('Prairie Creek @ Bruton/home/ubuntu/git/SensorDat.')
+prb = Cookbook('/home/ubuntu/git/SensorDat/example/prarie-creek-bruton.jsonld')
 prb.run()
 del prb
 
-print('Cottonwood Creek @ Cooks Lane...')
-ccc = Cookbook('../example/cottonwood_creek_cooks.jsonld')
+print('Cottonwood Creek @ Cooks Lane/home/ubuntu/git/SensorDat.')
+ccc = Cookbook('/home/ubuntu/git/SensorDat/example/cottonwood_creek_cooks.jsonld')
 ccc.run()
 del ccc
 
-print('Ballpark...')
-ball = Cookbook('../example/ballpark.jsonld')
+print('Ballpark/home/ubuntu/git/SensorDat.')
+ball = Cookbook('/home/ubuntu/git/SensorDat/example/ballpark.jsonld')
 ball.run()
 del ball
 
-print('Buckner...')
-buck = Cookbook('../example/buckner.jsonld')
+print('Buckner/home/ubuntu/git/SensorDat.')
+buck = Cookbook('/home/ubuntu/git/SensorDat/example/buckner.jsonld')
 buck.run()
 del buck
 
-print('Forest Lane East...')
-fle = Cookbook('../example/forest_lane_east.jsonld')
+print('Forest Lane East/home/ubuntu/git/SensorDat.')
+fle = Cookbook('/home/ubuntu/git/SensorDat/example/forest_lane_east.jsonld')
 fle.run()
 del fle
 
-print('Forest Lane West...')
-flw = Cookbook('../example/forest_lane_west.jsonld')
+print('Forest Lane West/home/ubuntu/git/SensorDat.')
+flw = Cookbook('/home/ubuntu/git/SensorDat/example/forest_lane_west.jsonld')
 flw.run()
 del flw
 
-print('Johnson Creek @ Duncan Perry...')
-jcdp = Cookbook('../example/johnson_creek_duncan_perry.jsonld')
+print('Johnson Creek @ Duncan Perry/home/ubuntu/git/SensorDat.')
+jcdp = Cookbook('/home/ubuntu/git/SensorDat/example/johnson_creek_duncan_perry.jsonld')
 jcdp.run()
 del jcdp
 
-print('Lake June...')
-ljun = Cookbook('../example/lake_june.jsonld')
+print('Lake June/home/ubuntu/git/SensorDat.')
+ljun = Cookbook('/home/ubuntu/git/SensorDat/example/lake_june.jsonld')
 ljun.run()
 del ljun
 
-print('White Rock Creek @ Royal...')
-wrc = Cookbook('../example/white_rock_creek_royal.jsonld')
+print('White Rock Creek @ Royal/home/ubuntu/git/SensorDat.')
+wrc = Cookbook('/home/ubuntu/git/SensorDat/example/white_rock_creek_royal.jsonld')
 wrc.run()
 del wrc
 
-print('Arlington North...')
-arn = Cookbook('../example/arlington_north.jsonld')
+print('Arlington North/home/ubuntu/git/SensorDat.')
+arn = Cookbook('/home/ubuntu/git/SensorDat/example/arlington_north.jsonld')
 arn.run()
 del arn
 
-print('Arlington West...')
-arw = Cookbook('../example/arlington_west.jsonld')
+print('Arlington West/home/ubuntu/git/SensorDat.')
+arw = Cookbook('/home/ubuntu/git/SensorDat/example/arlington_west.jsonld')
 arw.run()
 del arw
 
-print('Turtle Creek @ Blackburn...')
-tcb = Cookbook('../example/turtle_creek_blackburn.jsonld')
+print('Turtle Creek @ Blackburn/home/ubuntu/git/SensorDat.')
+tcb = Cookbook('/home/ubuntu/git/SensorDat/example/turtle_creek_blackburn.jsonld')
 tcb.run()
 del tcb
 
-print('Churchill Downs North...')
-cdn = Cookbook('../example/churchill-downs-north.jsonld')
+print('Churchill Downs North/home/ubuntu/git/SensorDat.')
+cdn = Cookbook('/home/ubuntu/git/SensorDat/example/churchill-downs-north.jsonld')
 cdn.run()
 del cdn
 
-print('Horseshoe Lake...')
-hl = Cookbook('../example/horseshoe_lake.jsonld')
+print('Horseshoe Lake/home/ubuntu/git/SensorDat.')
+hl = Cookbook('/home/ubuntu/git/SensorDat/example/horseshoe_lake.jsonld')
 hl.run()
 del hl
 
-print('Huron River @ Forest Ave...')
-hrf = Cookbook('../example/huron_river_forest_ave.jsonld')
+print('Huron River @ Forest Ave/home/ubuntu/git/SensorDat.')
+hrf = Cookbook('/home/ubuntu/git/SensorDat/example/huron_river_forest_ave.jsonld')
 hrf.run()
 del hrf
 
-print('Huron River @ Huron River Dr...')
-hrhr = Cookbook('../example/huron_river_huron_river.jsonld')
+print('Huron River @ Huron River Dr/home/ubuntu/git/SensorDat.')
+hrhr = Cookbook('/home/ubuntu/git/SensorDat/example/huron_river_huron_river.jsonld')
 hrhr.run()
 del hrhr
 
-print('Huron River @ Maiden Lane...')
-hrml = Cookbook('../example/huron_river_maiden_lane.jsonld')
+print('Huron River @ Maiden Lane/home/ubuntu/git/SensorDat.')
+hrml = Cookbook('/home/ubuntu/git/SensorDat/example/huron_river_maiden_lane.jsonld')
 hrml.run()
 del hrml
 
-print('Huron River @ Wagner...')
-hrw = Cookbook('../example/huron_river_wagner.jsonld')
+print('Huron River @ Wagner/home/ubuntu/git/SensorDat.')
+hrw = Cookbook('/home/ubuntu/git/SensorDat/example/huron_river_wagner.jsonld')
 hrw.run()
 del hrw
 
-print('Ice Cube...')
-ic = Cookbook('../example/ice_cube.jsonld')
+print('Ice Cube/home/ubuntu/git/SensorDat.')
+ic = Cookbook('/home/ubuntu/git/SensorDat/example/ice_cube.jsonld')
 ic.run()
 del ic
 
-print('Oxbow Lake...')
-ol = Cookbook('../example/oxbow_lake.jsonld')
+print('Oxbow Lake/home/ubuntu/git/SensorDat.')
+ol = Cookbook('/home/ubuntu/git/SensorDat/example/oxbow_lake.jsonld')
 ol.run()
 del ol
 
-print('South Commerce Lake...')
-scl = Cookbook('../example/south_commerce_lake.jsonld')
+print('South Commerce Lake/home/ubuntu/git/SensorDat.')
+scl = Cookbook('/home/ubuntu/git/SensorDat/example/south_commerce_lake.jsonld')
 scl.run()
 del scl
 
-print('Michigan Aero...')
-ma = Cookbook('../example/michigan-aero.jsonld')
+print('Michigan Aero/home/ubuntu/git/SensorDat.')
+ma = Cookbook('/home/ubuntu/git/SensorDat/example/michigan-aero.jsonld')
 ma.run()
 del ma
 
-print('Zingermans Roadhouse...')
-zr = Cookbook('../example/zingermans_roadhouse.jsonld')
+print('Zingermans Roadhouse/home/ubuntu/git/SensorDat.')
+zr = Cookbook('/home/ubuntu/git/SensorDat/example/zingermans_roadhouse.jsonld')
 zr.run()
 del zr
 
-print('Herbarium...')
-herb = Cookbook('../example/herbarium.jsonld')
+print('Herbarium/home/ubuntu/git/SensorDat.')
+herb = Cookbook('/home/ubuntu/git/SensorDat/example/herbarium.jsonld')
 herb.run()
 del herb
 
-print('OHM Rain Garden...')
-ohm = Cookbook('../example/ohm_rain_garden.jsonld')
+print('OHM Rain Garden/home/ubuntu/git/SensorDat.')
+ohm = Cookbook('/home/ubuntu/git/SensorDat/example/ohm_rain_garden.jsonld')
 ohm.run()
 del ohm
 
-print('Benton Harbor Empire...')
-bhem = Cookbook('../example/benton_harbor_empire.jsonld')
+print('Benton Harbor Empire/home/ubuntu/git/SensorDat.')
+bhem = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_empire.jsonld')
 bhem.run()
 del bhem
 
-print('Benton Harbor Euclid...')
-bheu = Cookbook('../example/benton_harbor_euclid.jsonld')
+print('Benton Harbor Euclid/home/ubuntu/git/SensorDat.')
+bheu = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_euclid.jsonld')
 bheu.run()
 del bheu
 
-print('Benton Harbor Meadowbrook...')
-bhm = Cookbook('../example/benton_harbor_meadowbrook.jsonld')
+print('Benton Harbor Meadowbrook/home/ubuntu/git/SensorDat.')
+bhm = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_meadowbrook.jsonld')
 bhm.run()
 del bhm
 
-print('Benton Harbor Napier...')
-bhn = Cookbook('../example/benton_harbor_napier.jsonld')
+print('Benton Harbor Napier/home/ubuntu/git/SensorDat.')
+bhn = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_napier.jsonld')
 bhn.run()
 del bhn
 
-print('Benton Harbor Pearl Grange...')
-bhpg = Cookbook('../example/benton_harbor_pearl_grange.jsonld')
+print('Benton Harbor Pearl Grange/home/ubuntu/git/SensorDat.')
+bhpg = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_pearl_grange.jsonld')
 bhpg.run()
 del bhpg
 
-print('Benton Harbor Pipestone...')
-bhps = Cookbook('../example/benton_harbor_pipestone.jsonld')
+print('Benton Harbor Pipestone/home/ubuntu/git/SensorDat.')
+bhps = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_pipestone.jsonld')
 bhps.run()
 del bhps
 
-print('Benton Harbor Water...')
-bhw = Cookbook('../example/benton_harbor_water.jsonld')
+print('Benton Harbor Water/home/ubuntu/git/SensorDat.')
+bhw = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_water.jsonld')
 bhw.run()
 del bhw
 
-print('Benton Harbor Yore...')
-bhy = Cookbook('../example/benton_harbor_yore.jsonld')
+print('Benton Harbor Yore/home/ubuntu/git/SensorDat.')
+bhy = Cookbook('/home/ubuntu/git/SensorDat/example/benton_harbor_yore.jsonld')
 bhy.run()
 del bhy
